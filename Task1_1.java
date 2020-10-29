61a8468db2a612e11c63e89e6a5c35155d28be51
corebrest <73549603+corebrest@users.noreply.github.com>
2020-10-29 14:34:54 +0300

    Task 1.1
    Basics of software code development

---
+++ Task1_1.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Task1_1 {
+    public static void main(String[] args) {
+        int a = 7;
+        double b = 6.0;
+        int c = 1;
+        double z = ((a - 3)* b / 2) + c;
+        System.out.println(z);
+    }
+}
+
+
+
